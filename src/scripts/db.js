559f667640a1f092f395808b4852edddcb0c542d
Aaron Powell <me@aaron-powell.com>
2020-08-14 14:37:01 +1000

    adding some storage for the selected tz's

---
+++ src/scripts/db.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const storage = localStorage;
+
+const setItem = (key, value) => {
+  storage.setItem(key, JSON.stringify(value));
+  return Promise.resolve();
+};
+
+const getItem = (key, defaultValue = undefined) => {
+  const item = storage.getItem(key) || defaultValue;
+  return Promise.resolve(typeof item === typeof "" ? JSON.parse(item) : item);
+};
+
+export { setItem, getItem };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,17 +3,22 @@
 
   const { createPicker } = await import("./timezone-picker.js");
   const { formatTimeZone } = await import("./utils.js");
+  const db = await import("./db.js");
 
   const addTimeZoneButton = document.getElementById("addTimeZone");
   addTimeZoneButton.addEventListener("click", () => {
     const select = createPicker(window.moment, document);
-    select.addEventListener("zoneSelected", ({ zoneInfo }) => {
+    select.addEventListener("zoneSelected", async ({ zoneInfo }) => {
       const p = document.createElement("p");
       p.innerHTML = `${zoneInfo.name} - UTC ${formatTimeZone(
         zoneInfo.offsetHours
       )}`;
 
       document.getElementById("main").appendChild(p);
+
+      const knownZones = await db.getItem("zones", []);
+
+      await db.setItem("zones", knownZones.concat([zoneInfo]));
     });
 
     document.getElementById("main").appendChild(select);
