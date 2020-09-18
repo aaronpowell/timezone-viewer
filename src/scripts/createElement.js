d0710b4ef5568fa61a744f98feb20dab68909550
Aaron Powell <me@aaron-powell.com>
2020-09-18 13:04:22 +1000

    shifting the element creation into a more scheduled job

--- src/scripts/createElement.js
+++ src/scripts/createElement.js
@@ -1,36 +1,30 @@
 const eventRegex = /^on/;
 
-const render = ({ type, tag, props, children }, renderTarget) => {
-  let element;
+const createDom = ({ type, tag, props }) => {
+  const element =
+    type === "TEXT" ? document.createTextNode("") : document.createElement(tag);
 
-  if (type === "NOOP") {
-    for (const child of children) {
-      render(child, renderTarget);
-    }
-  } else {
-    element =
-      type === "TEXT"
-        ? document.createTextNode("")
-        : document.createElement(tag);
+  Object.keys(props)
+    .filter((key) => !key.startsWith("on"))
+    .forEach((key) => (element[key] = props[key]));
 
-    Object.keys(props)
-      .filter((key) => !key.startsWith("on"))
-      .forEach((key) => (element[key] = props[key]));
+  Object.keys(props)
+    .filter((key) => eventRegex.test(key))
+    .forEach((event) =>
+      element.addEventListener(
+        event.replace(eventRegex, "").toLowerCase(),
+        props[event]
+      )
+    );
 
-    Object.keys(props)
-      .filter((key) => eventRegex.test(key))
-      .forEach((event) =>
-        element.addEventListener(
-          event.replace(eventRegex, "").toLowerCase(),
-          props[event]
-        )
-      );
+  return element;
+};
 
-    for (const child of children) {
-      render(child, element);
-    }
-    renderTarget.appendChild(element);
-  }
+const render = (element, renderTarget) => {
+  nextUnitOfWork = {
+    dom: renderTarget,
+    children: [element],
+  };
 };
 
 const createTextElement = (text) => {
@@ -45,9 +39,9 @@ const createTextElement = (text) => {
 
 const createElement = (tag, props, ...children) => {
   const descriptor = {
-    type: !tag ? "NOOP" : "ELEMENT",
-    tag: tag,
-    props: props,
+    type: "ELEMENT",
+    tag,
+    props,
     children: children.map((child) =>
       typeof child === "object" ? child : createTextElement(child)
     ),
@@ -55,5 +49,60 @@ const createElement = (tag, props, ...children) => {
 
   return descriptor;
 };
+
+const performUnitOfWork = (currentUnitOfWork) => {
+  if (!currentUnitOfWork.dom) {
+    currentUnitOfWork.dom = createDom(currentUnitOfWork);
+  }
+
+  if (currentUnitOfWork.parent) {
+    currentUnitOfWork.parent.dom.appendChild(currentUnitOfWork.dom);
+  }
+
+  const children = currentUnitOfWork.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < children.length) {
+    const child = children[index];
+    const uow = {
+      ...child,
+      parent: currentUnitOfWork,
+      dom: null,
+    };
+
+    if (index === 0) {
+      currentUnitOfWork.child = uow;
+    } else {
+      prevSibling.sibling = uow;
+    }
+
+    prevSibling = uow;
+    index++;
+  }
+
+  if (currentUnitOfWork.child) {
+    return currentUnitOfWork.child;
+  }
+
+  let nextUnitOfWork = currentUnitOfWork;
+  while (nextUnitOfWork) {
+    if (nextUnitOfWork.sibling) {
+      return nextUnitOfWork.sibling;
+    }
+    nextUnitOfWork = nextUnitOfWork.parent;
+  }
+};
+
+let nextUnitOfWork = null;
+const workLoop = (deadline) => {
+  let shouldStopWork = false;
+  while (nextUnitOfWork && !shouldStopWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldStopWork = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+};
+requestIdleCallback(workLoop);
 
 export { render, createElement };
